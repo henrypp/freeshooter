31b3080a1aaccc854140a875a4ee221b5e843d54
henrypp <support@henrypp.org>
2018-10-22 13:13:14 +0600

    upd version string & copyright

--- src/app.hpp
+++ src/app.hpp
@@ -1,13 +1,13 @@
 // Free Shooter
-// Copyright (c) 2009, 2010, 2018 Henry++
+// Copyright (c) 2009-2018 Henry++
 
 #ifndef __APP_H__
 #define __APP_H__
 
 #define APP_NAME L"Free Shooter"
 #define APP_NAME_SHORT L"freeshooter"
-#define APP_VERSION L"2.0.6"
-#define APP_VERSION_RES 2,0,6,0
-#define APP_COPYRIGHT L"(c) 2009, 2010, 2018 " _APP_AUTHOR L". All Rights Reversed."
+#define APP_VERSION L"2.0.7"
+#define APP_VERSION_RES 2,0,7,0
+#define APP_COPYRIGHT L"(c) 2009-2018 " _APP_AUTHOR L". All Rights Reversed."
 
 #endif // __APP_H__
