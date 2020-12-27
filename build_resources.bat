eaabe69b9fc7eb1998b72fff2ed2987f724d1a1b
henrypp <support@henrypp.org>
2020-12-27 16:02:29 +0600

    fix resource build script

--- build_resources.bat
+++ build_resources.bat
@@ -1,7 +1,7 @@
 @echo off
 
-php "..\builder\make_resource.php" ".\src\resource.hpp"
-php "..\builder\make_locale.php" "Free Shooter" "freeshooter" ".\bin\i18n" ".\src\resource.hpp" ".\src\resource.rc" ".\bin\freeshooter.lng"
+php "..\builder\make_resource.php" ".\src\resource.h"
+php "..\builder\make_locale.php" "Free Shooter" "freeshooter" ".\bin\i18n" ".\src\resource.h" ".\src\resource.rc" ".\bin\freeshooter.lng"
 copy /y ".\bin\freeshooter.lng" ".\bin\32\freeshooter.lng"
 copy /y ".\bin\freeshooter.lng" ".\bin\64\freeshooter.lng"
 
