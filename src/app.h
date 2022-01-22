4cbc43077d6cab30e5c3feec062f0dafbb26740f
henrypp <support@henrypp.org>
2022-01-22 22:05:16 +0600

    ++year

--- src/app.h
+++ src/app.h
@@ -1,5 +1,5 @@
 // Free Shooter
-// Copyright (c) 2009-2021 Henry++
+// Copyright (c) 2009-2022 Henry++
 
 #pragma once
 
@@ -8,4 +8,4 @@
 #define APP_VERSION L"2.1"
 #define APP_VERSION_RES 2,1,0,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2009-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2009-2022 " APP_AUTHOR L". All Rights Reversed."
