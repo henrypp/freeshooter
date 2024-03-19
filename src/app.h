7a0d9b3d8c4d88d3c020c43737129486796776dc
henrypp <sforce5@mail.ru>
2024-03-20 04:41:09 +0500

    upd version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"Free Shooter"
 #define APP_NAME_SHORT L"freeshooter"
-#define APP_VERSION L"2.1.1"
-#define APP_VERSION_RES 2,1,1,0
+#define APP_VERSION L"2.2"
+#define APP_VERSION_RES 2,2,0,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2009-2024 " APP_AUTHOR L". All Rights Reversed."
