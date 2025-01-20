bec9fa5ee8bfec6224b60bb0b62ed777f24ca58c
henrypp <sforce5@mail.ru>
2025-01-20 19:30:59 +0500

    ++version

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
 // Free Shooter
-// Copyright (c) 2009-2024 Henry++
+// Copyright (c) 2009-2025 Henry++
 
 #pragma once
 
 #define APP_NAME L"Free Shooter"
 #define APP_NAME_SHORT L"freeshooter"
-#define APP_VERSION L"2.2"
-#define APP_VERSION_RES 2,2,0,0
+#define APP_VERSION L"2.2.1"
+#define APP_VERSION_RES 2,2,1,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2009-2024 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2009-2025 " APP_AUTHOR L". All Rights Reversed."
